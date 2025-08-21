66f836a02940de6b09e938e50d13c5408fd975a5
rafiabdulmugni757-ops <rafiabdulmugni757@gmail.com>
2025-08-21 20:27:56 +0700

    Update insert_sample_data.sql

--- insert_sample_data.sql
+++ insert_sample_data.sql
@@ -1,13 +1,14 @@
 -- Insert initial sample data
 INSERT INTO restaurant (name, street_address, description) VALUES
-('Warung Sederhana', 'Jl. Mawar No. 10, Sukabumi', 'Masakan rumahan khas Minang.'),
-('Bakso Mantap', 'Jl. Kenanga No. 5, Sukabumi', 'Spesialis bakso urat dan kuah gurih.'),
-('Nasi Goreng Nusantara', 'Jl. Melati No. 20, Sukabumi', 'Aneka nasi goreng dengan topping lokal.');
+('Warung Mang Saleh', 'Jl. Mita No. 16 Sukabumi', 'Masakan khas Sunda.'),
+('Mie Ayam Lajur', 'Jl. Lajur No. 2, Cianjur', 'Mie Ayam Dengan Bumbu Turun Temurun.'),
+('Seblak Parasmanan Yandi', 'Jl. Bayangkara No. 11, Sukabumi', 'Seblak Parasmanan Dengan Kuah Yang Medok.');
 
 -- 5 reviews
 INSERT INTO review (restaurant_id, user_name, rating, review_text, review_date) VALUES
-(1, 'Alya', 5, 'Rasanya otentik dan porsinya besar.', '2025-07-10'),
-(1, 'Budi', 4, 'Enak, tapi agak ramai saat makan siang.', '2025-07-12'),
-(2, 'Citra', 5, 'Baksonya kenyal, kuahnya mantap!', '2025-07-15'),
-(2, 'Dedi', 3, 'Harga oke, tapi tempatnya sempit.', '2025-07-18'),
-(3, 'Eka', 4, 'Nasi goreng kambingnya top.', '2025-07-20');
+(1, 'Dinda', 5, 'Rasanya Enak dan mantap.', '2025-07-10'),
+(1, 'Abdul', 4, 'Enak, tapi suka abis kalo udh mau magrib.', '2025-07-12'),
+(2, 'Rafi', 5, 'Bumbunya mantap!', '2025-07-15'),
+(2, 'Adi', 3, 'Harga murah banget, tapi jaraknya jauh.', '2025-07-18'),
+(3, 'Alya', 4, 'makanannya top markotop.', '2025-07-20');
+
